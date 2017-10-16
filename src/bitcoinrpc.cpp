452063d0fb37ff9011f3c511b5edafed24009450
FalconCoin <ebtclassic@gmail.com>
2017-10-16 08:36:12 +0530

    EBT V1.02

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -39,7 +39,7 @@ static boost::thread_group* rpc_worker_group = NULL;
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 18616 : 8616; //Testnet, RPC Port
+    return GetBoolArg("-testnet", false) ? 18116 : 8116; //Testnet, RPC Port
 }
 
 Object JSONRPCError(int code, const string& message)
